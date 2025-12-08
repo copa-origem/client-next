90b3a767b7aceecb9b0aac837e101a7d639d5eef
Rafael Rangel <rafael.profissional011@gmail.com>
2025-12-08 11:44:43 -0300

    ajustando aba

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ const _geistMono = Geist_Mono({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: 'Alerta Cidadão',
   icons: {
-    icon: '../assets/icon.png',
+    icon: '/icon.png',
   },
 }
 
