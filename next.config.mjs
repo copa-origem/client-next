a3732fe998f66f03f2bfd5acd6decd38cd03c8fd
Rafael Rangel <rafinhad3las@gmail.com>
2026-01-14 07:54:05 -0300

    refactor: deleting the old proxy

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: true, 
   },
   images: {
     unoptimized: true,
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://20.63.25.230:3000/:path*',
-      },
-    ];
   },
 };
 
